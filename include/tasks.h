0b143d522be4c51ce0f581d7638e1fc539e16cfd
Nikita <nikita200604prochorov@mail.ru>
2025-05-14 23:46:06 +0300

    project

---
+++ include/tasks.h
@@ -0,0 +1,11 @@
+// Copyright 2025 UNN-CS Team
+
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+#include <vector>
+
+double calculatEarthandRope();
+std::vector<double> calculatePool();
+
+#endif  // INCLUDE_TASKS_H_
